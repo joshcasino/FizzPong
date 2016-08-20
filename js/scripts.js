f754dfbdda4a8e0bb06485a53690738b0a2fa0ae
Josh Casuccio <joshcasino@gmail.com>
2016-08-20 12:39:20 -0700

    add final javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,32 @@
 //business logic
-function pingPong(num) {
-	for (i = 1; i <= num; i++) {
-		if (i % 15 === 0) {
-			return "ping pong";
+$(document).ready(function() {
+	var pingPong = function(num) {
+		var pongArray = [];
+		for (i = 1; i <= num; i++) {
+			if (i % 15 === 0) {
+				pongArray.push("ping pong");
+			}
+		  else if  (i % 5 === 0) {
+				pongArray.push("pong");
+			}
+		  else if (i % 3 === 0) {
+				pongArray.push("ping");
+			}
+		  else {
+			  pongArray.push(i);
+			}
 		}
-    else if  (i % 5 === 0) {
-			return "pong";
-		}
-    else if (i % 3 === 0) {
-			return "ping";
-		}
-    else {
-		    return i;
-		}
+		return pongArray;
 	}
-}
 
 //UI Logic
-$(document).ready(function() {
-  $("#pongForm").submit(function(event) {
-    var pongInput = parseInt($("#pongInput").val());
-        alert(pongInput)
-    var pongResult = pingPong(pongInput);
-    $("#pongResult").text(pongResult);
-    event.preventDefault();
+
+$("#pongForm").submit(function(event) {
+  var pongInput = parseInt($("#pongInput").val());
+  var pongResult = pingPong(pongInput);
+	pongResult.forEach(function(pongItem) {
+		$("ul").append("<li>" + pongItem + "</li>");
+	});
+  event.preventDefault();
   });
 });
-
-  // function romanConvert(num) {
-  //   // alert(num);
-  //   if (typeof num !== 'number')
-  //   return false;
-  //   var digits = String(+num).split(""),
-
-  //   roman_num += (key[+digits.pop() + (i * 10)] || "");
-  //   return Array(+digits.join("") + 1).join("M") + roman_num;
-  // }
-  //
-  // $(document).ready(function() {
-  //   $("#romanForm").submit(function(event) {
-  //     var rInput = parseInt($("#romanInput").val());
-  //     var rResult = romanConvert(rInput);
-  //     $("#romanResult").text(rResult);
-  //       event.preventDefault();
-  //   });
